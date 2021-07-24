a1c5b83da3dab191ad8bf4f62a767a2e7cd265ae
TimWeyel <tweyel@gmail.com>
2021-07-23 18:00:21 -0700

    changing database settinsg

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tweyel:gcFF83u18dTN@cluster0.fyzii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -8,3 +8,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
 });
 
 module.exports = mongoose.connection;
+
+
