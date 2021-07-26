e62b7e8aa448389753b5b4f32029f3c51f7521a3
TimWeyel <tweyel@gmail.com>
2021-07-26 12:19:30 -0700

    updating db connections

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tweyel:gcFF83u18dTN@cluster0.fyzii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
